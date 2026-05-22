cbe5daba75ee12d5e95d7ea24b0d9ea51cfe95b3
Aleks Kholod <holodsasha170804@gmail.com>
2026-05-22 13:10:56 +0200

    this is test commit from home PC

--- src/components/SearchPanel.ts
+++ src/components/SearchPanel.ts
@@ -1,14 +1,38 @@
+import data from '@/data/categories.json';
+
 export default function SearchPanel(): string {
     return `
     <div class="container search-panel">
-        <div class="search-panel__body">
-            <button type="button" class="button search-panel__button-categories icon__before icon__before--white-arrow-caret-down-fill">Alle Kategorien</button>
-            <input type="text" class="search-panel__input" placeholder="Suche in der Publicationen..."/>
-            <button type="button" class="search-panel__button-search button icon__before icon__before--white-search">Suchen</button>
-        </div>
-        <div class="search-panel__select-category categories">
-       
-        </div>
+        <form class="search-panel__body">
+            <div class="search-panel__panel">
+                <button type="button" class="button search-panel__button-categories icon__before icon__before--white-arrow-caret-down-fill" data-ts-categories-button>Alle Kategorien</button>
+                <input name="search" class="search-panel__input" placeholder="Suche in der Publicationen..."/>
+                <button type="submit" class="search-panel__button-search button icon__before icon__before--white-search">Suchen</button> 
+            </div>
+            <div class="search-panel__select-categories categories">
+                ${renderCategories()}
+            </div>
+        </form>
     </div>
     `
 }
+
+function renderCategories(): string {
+    return `
+    <ul class="categories__list" data-ts-categories>
+        ${Object.entries(data.categories)
+            .map(([key, category])=> {
+                return `
+                    <li class="categories__item">
+                        <button type="button" class="categories__title icon__after icon__after--chevron-right">
+                            ${key + ' ' + category.name}
+                        </button>
+                    </li>
+                `
+            })
+            .join('')
+        }
+    </ul>
+    `;
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import '@/assets/styles/main.scss';
 import App from "@/app/App.ts";
+import CategoriesButton from "@/components/CategoriesButton.ts";
 
 const root = document.querySelector('#app');
 
 if (root) {
     root.innerHTML = App();
 }
+
+new CategoriesButton();
